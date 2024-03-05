800065261a26a117ee72944ddbc62327de3be521
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-03-06 00:43:15 +0300

    sending email and veryfying user email

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,8 +1,35 @@
 "use client";
 import {BeatLoader,MoonLoader,PacmanLoader,ClimbingBoxLoader,HashLoader,RotateLoader
 } from "react-spinners"
+import { useCallback,useEffect,useState} from "react";
 import {CardWrapper} from "@/components/auth/card-wrapper"
+import { useSearchParams } from "next/navigation";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+import { NewVerification } from "@/actions/new-verification";
 export const NewVerificationForm = ()=>{
+    const searchParams = useSearchParams()
+    const [error,setError] = useState<string | undefined>()
+    const [success,setSuccess] = useState<string | undefined>()
+    const token = searchParams.get("token")
+    const onSubmit = useCallback(()=>{
+        if(!token) {
+            setError("Missing token!")
+            return
+        }
+       NewVerification(token).then((data)=>{
+        setSuccess(data.success)
+        setError(data.error)
+       }).catch(()=>{
+        setError("something went wrong")
+       })
+
+    },[token])
+    useEffect(()=>{
+      onSubmit()
+    },[onSubmit])
+
+    
     return(
         <CardWrapper
         headerLabel="Confirming your verification"
@@ -10,10 +37,10 @@ export const NewVerificationForm = ()=>{
         backButtonHref="/auth/login"
         >
             <div className="flex items-center w-full justify-center">
-                <RotateLoader
-
+                {!success && !error && <HashLoader/> }
                 
-                />
+                <FormSuccess message={success}></FormSuccess>
+                <FormError message={error}></FormError>
             </div>
         </CardWrapper>
     )
