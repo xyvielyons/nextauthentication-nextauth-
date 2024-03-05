eb2f6f79b2223d4bab9965c9a2c11a78ae5a4e38
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-03-05 10:04:32 +0300

    server side log out button

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,9 +1,18 @@
-import { auth } from "@/auth"
+import { auth,signOut } from "@/auth"
+import { Button } from "@/components/ui/button"
 export default async function SettingsPage() {
 const session = await auth()
   return (
     <div>
         {JSON.stringify(session)}
+        <form action={async ()=>{
+          "use server"
+          await signOut()
+        }}>
+          <Button type="submit" variant="outline">
+            Sign out
+          </Button>
+        </form>
     </div>
   )
 }
