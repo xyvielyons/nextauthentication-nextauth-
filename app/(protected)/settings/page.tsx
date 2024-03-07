9092915c20ec0377e84a83af07dc971edb9e935a
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-03-07 11:39:04 +0300

    2 factor confirmation ()

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -2,7 +2,6 @@ import { auth,signOut } from "@/auth"
 import { Button } from "@/components/ui/button"
 export default async function SettingsPage() {
 const session = await auth()
-session?.user
   return (
     <div>
         {JSON.stringify(session)}
