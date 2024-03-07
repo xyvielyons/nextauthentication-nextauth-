051a91ac94587235ed1392e8d5f24949db9b4e39
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-03-07 11:57:02 +0300

    minify user data in settings page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,18 +1,24 @@
-import { auth,signOut } from "@/auth"
-import { Button } from "@/components/ui/button"
-export default async function SettingsPage() {
-const session = await auth()
+"use client"
+
+import { useCurrentUser } from "@/hooks/use-current-user"
+import { useSession,signOut } from "next-auth/react"
+
+export default function SettingsPage() {
+  //server
+// const session = await auth()
+
+//client
+// const session = useSession()
+
+const user = useCurrentUser()
+const onClick=()=>{
+  signOut()
+}
+
   return (
     <div>
-        {JSON.stringify(session)}
-        <form action={async ()=>{
-          "use server"
-          await signOut()
-        }}>
-          <Button type="submit" variant="outline">
-            Sign out
-          </Button>
-        </form>
+        {JSON.stringify(user)}
+        <button onClick={onClick}>Sign Out</button>
     </div>
   )
 }
