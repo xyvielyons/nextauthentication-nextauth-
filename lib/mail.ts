7f40477e63b0747a8d9eca078a620deed39a22aa
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-03-07 19:07:11 +0300

    minor changes

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,5 +1,6 @@
 import {Resend} from "resend"
 const resend = new Resend(process.env.RESEND_API_KEY)
+const domain = process.env.NEXT_PUBLIC_APP_URL
 export const sendTwoFactorTokenEmail = async(
     email:string,
     token:string
@@ -17,7 +18,7 @@ export const sendVerificationEmail = async(
     email:string,
     token:string
 )=>{
-    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`
+    const confirmLink = `${domain}/auth/new-verification?token=${token}`
     await resend.emails.send({
         from:"onboarding@resend.dev",
         to:email,
