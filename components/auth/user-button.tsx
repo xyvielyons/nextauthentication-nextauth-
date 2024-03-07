2d7a277123796ecc6075edb183b85315704d0c3d
Twisted-shadow <111194732+Twisted-shadow@users.noreply.github.com>
2024-03-07 12:51:01 +0300

    complete navbar implementation

--- components/auth/user-button.tsx
+++ components/auth/user-button.tsx
@@ -15,6 +15,7 @@ AvatarImage
 } from "@/components/ui/avatar"
 import { useCurrentUser } from "@/hooks/use-current-user"
 import { LogoutButton } from "@/components/auth/logout-button"
+import { ExitIcon } from "@radix-ui/react-icons"
 
 export const UserButton = ()=>{
     const user = useCurrentUser()
@@ -29,9 +30,10 @@ export const UserButton = ()=>{
                 </AvatarFallback>
                 </Avatar>
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
+            <DropdownMenuContent className="w-40" align="end">
                 <LogoutButton>
                     <DropdownMenuItem>
+                        <ExitIcon className="h-4 w-4 mr-2"></ExitIcon>
                         Logout
                     </DropdownMenuItem>
 
